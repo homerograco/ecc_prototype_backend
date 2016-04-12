a8cb9fa6e039ead1c147b0a315a1533b15050458
Homero Graco <homero.graco@gmail.com>
2016-04-12 17:53:19 +0200

    separated db of questions and answers and created proper routes

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 // Routes
 
+/*
 $app->get('/all/', function ($request, $response) {
     // Logging read of ECC codes table
     $this->logger->info("Reading whole table of ECC codes");
@@ -12,17 +13,57 @@ $app->get('/all/', function ($request, $response) {
     $newResponse->getBody()->write($json);
     return $newResponse;
 });
+ * 
+ */
 
-$app->get('/all/{id}', function ($request, $response, $args) {
-    // Logging read of ECC codes table
-    $this->logger->info("Reading ECC code by id");
+//More general GET request
+$app->get('/all/[{id}/]', function ($request, $response, $args) {
+
+    $this->logger->info("Reading ECC codes");
     
-    $ecc_id = $args['id'];
+    if (isset($args['id'])) {
+        $ecc_id = $args['id'];
+        $table = R::findOne('ecc_codes', 'id=?', array($ecc_id));
+    } else {
+        $table = R::find('ecc_codes');
+    }
     
-    $table = R::findOne('ecc_codes', 'id=?', array($ecc_id));
     $json = json_encode(array('results' => R::exportAll($table)));
     $newResponse = $response->withHeader('Content-type', 'application/json');
-   // $newResponse->getBody()->write(var_export($json, true));
+    $newResponse->getBody()->write($json);
+    return $newResponse;
+});
+
+$app->get('/answers/[{id}/]', function ($request, $response, $args) {
+    
+    $this->logger->info("Reading codes-answers table");
+    
+    if (isset($args['id'])) {
+        $ecc_id = $args['id'];
+        $table = R::findOne('code_answer', 'id=?', array($ecc_id));
+    } else {
+        $table = R::find('code_answer');
+    }
+    
+    $json = json_encode(array('results' => R::exportAll($table)));
+    $newResponse = $response->withHeader('Content-type', 'application/json');
+    $newResponse->getBody()->write($json);
+    return $newResponse;
+});
+
+$app->get('/questions/[{id}/]', function ($request, $response, $args) {
+    // Logging read of ECC codes table
+    $this->logger->info("Reading codes-questions table");
+    
+    if (isset($args['id'])) {
+        $ecc_id = $args['id'];
+        $table = R::findOne('code_question', 'id=?', array($ecc_id));
+    } else {
+        $table = R::find('code_question');
+    }
+    
+    $json = json_encode(array('results' => R::exportAll($table)));
+    $newResponse = $response->withHeader('Content-type', 'application/json');
     $newResponse->getBody()->write($json);
     return $newResponse;
 });
