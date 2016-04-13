cc56dd0675162bcfdfa13658a16328bc5bd228da
Homero Graco <homero.graco@gmail.com>
2016-04-13 14:22:57 +0200

    Updated routes to GET separate DBs of questions and answers into an associative array, and return it as JSON.

--- src/routes.php
+++ src/routes.php
@@ -1,22 +1,9 @@
 <?php
 // Routes
 
-/*
-$app->get('/all/', function ($request, $response) {
-    // Logging read of ECC codes table
-    $this->logger->info("Reading whole table of ECC codes");
-    
-    $table = R::find('ecc_codes');
-    $json = json_encode(array('results' => R::exportAll($table)));
-    $newResponse = $response->withHeader('Content-type', 'application/json');
-    //$newResponse->getBody()->write(var_export($json, true));
-    $newResponse->getBody()->write($json);
-    return $newResponse;
-});
- * 
+/**
+ * OLD: Reads database of ECC information into an associative array and returns it as json
  */
-
-//More general GET request
 $app->get('/all/[{id}/]', function ($request, $response, $args) {
 
     $this->logger->info("Reading ECC codes");
@@ -34,6 +21,9 @@ $app->get('/all/[{id}/]', function ($request, $response, $args) {
     return $newResponse;
 });
 
+/**
+ * Reads database of answers into an associative array and returns it as json
+ */
 $app->get('/answers/[{id}/]', function ($request, $response, $args) {
     
     $this->logger->info("Reading codes-answers table");
@@ -51,6 +41,9 @@ $app->get('/answers/[{id}/]', function ($request, $response, $args) {
     return $newResponse;
 });
 
+/**
+ * Reads database of questions into an associative array and returns it as json
+ */
 $app->get('/questions/[{id}/]', function ($request, $response, $args) {
     // Logging read of ECC codes table
     $this->logger->info("Reading codes-questions table");
@@ -63,6 +56,43 @@ $app->get('/questions/[{id}/]', function ($request, $response, $args) {
     }
     
     $json = json_encode(array('results' => R::exportAll($table)));
+    $newResponse = $response->withHeader('Content-type', 'application/json');
+    $newResponse->getBody()->write($json);
+    return $newResponse;
+});
+
+/**
+ * Combines databases of questions and answers into an associative array and returns it as json
+ */
+$app->get('/questions_answers/[{code}/]', function ($request, $response, $args) {
+    // Logging read of ECC codes table
+    $this->logger->info("Testing join of questions X answers");
+    
+    if (isset($args['code'])) {
+        $code = $args['code'];
+        $question = R::findOne('code_question', 'code=?', array($code));
+        $answers = R::find('code_answer', ' code LIKE ? ', [ $code ]);
+        $results = array();
+        
+        foreach($answers as $answer) {
+            $results[$question->code][$question->question][] = array($answer->next => $answer->answer);
+        }
+        
+    } else {
+        $questions = R::find('code_question');
+        $answers = R::find('code_answer');
+        $results = array();
+    
+        foreach($questions as $question) {
+            foreach($answers as $answer) {
+                if($answer->code == $question->code) {
+                    $results[$question->code][$question->question][] = array($answer->next => $answer->answer);
+                }
+            }
+        }
+    }
+
+    $json = json_encode(array('results' => $results));
     $newResponse = $response->withHeader('Content-type', 'application/json');
     $newResponse->getBody()->write($json);
     return $newResponse;
